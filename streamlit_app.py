0c8533be336d857d3ca3f81e9d8fe7523ee6356c
injnovakov-create <inj.novakov@gmail.com>
2026-03-17 13:31:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ import json  # НОВО: За работа със запис/зареждане 
 import urllib.request
 from PIL import Image, ImageDraw, ImageFont
 
-# Настройки на страницата
-st.set_page_config(page_title="SMART CUT: Витя-М", layout="wide")
+# Настройки на страницата - Сменяме името и тук
+st.set_page_config(page_title="OPTIVIK: Витя-М", layout="wide")
 
 # --- CSS ЗА СБИТ ДИЗАЙН ---
 st.markdown("""
@@ -20,7 +20,7 @@ html { zoom: 0.95; }
 .block-container { padding-top: 2.5rem !important; padding-bottom: 1.5rem !important; }
 h1, h2, h3, h4, h5 { padding-top: 0.3rem !important; padding-bottom: 0.3rem !important; margin-bottom: 0 !important; }
 
-/* СТИЛ ЗА НОВАТА МАРКА */
+/* СТИЛ ЗА НОВАТА МАРКА OPTIVIK */
 .opti-text { color: #000000; font-weight: bold; }
 .vik-text { 
     color: #FF0000; 

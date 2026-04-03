8ea39821aba6c77dd6d5b33db5ad4034e19b745f
injnovakov-create <inj.novakov@gmail.com>
2026-04-03 06:41:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -392,7 +392,7 @@ with col1:
                     vrati_broi = st.radio("Брой врати:", [1, 2], index=1 if w > 500 else 0, horizontal=True, key="vr_low")
 
         st.markdown("---")
-                    vrati_broi = st.radio("Брой врати:", [1, 2], index=1 if w > 500 else 0, horizontal=True, key="vr_low")
+        vrati_broi = st.radio("Брой врати:", [1, 2], index=1 if w > 500 else 0, horizontal=True, key="vr_low")
                 else:
                 h_box = st.number_input("Височина на корпуса без крака (мм)", value=760, key="h_box_low")
                 d = st.number_input("Дълбочина (D) мм", value=(550 if tip == "Шкаф Мивка" else 520), key="d_low")
